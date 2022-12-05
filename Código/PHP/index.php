630f6c03081c070770a713c9895b85394c50e738
Guillermo González Martínez <114804359+WillyUAH@users.noreply.github.com>
2022-12-05 13:27:08 +0100

    index sin basura

--- Código/PHP/index.php
+++ Código/PHP/index.php
@@ -8,17 +8,6 @@ require_once($dir_raiz."includes/config.php");
 require_once($dir_raiz."includes/funciones.php");
 require_once($dir_raiz."includes/encabezado.php");
 
-/*
-$sql_connect = conectar_bd();
-$sql = "select * from venta"; // Consulta SQL
-$query = $sql_connect->query($sql); // Ejecutar la consulta SQL
-$data1 = array(); // Array donde vamos a guardar los datos
-while($r = $query->fetch_object()){ // Recorrer los resultados de Ejecutar la consulta SQL
-    $data1[]=$r; // Guardar los resultados en la variable $data
-}
-desconectar_bd($sql_connect);
-writeLog($sql);
-*/
 require_once($dir_raiz."includes/cabecera.php");
 
 ?>
@@ -41,47 +30,7 @@ require_once($dir_raiz."includes/cabecera.php");
     },26000);
 </script>
 
-<!--
-<script src="js/chartjs/chart.js"></script>
-<canvas id="grafico1" style="width:100%;" height="300"></canvas>
 
-<script>
-
-    var ctx1 = document.getElementById("grafico1");
-    var data1 = {
-            labels: [ 
-            <?/*php foreach($data1 as $d):?>
-                "<?php echo $d->date_at?>", 
-            <?php endforeach; */?>
-            ],
-            datasets: [{
-                label: '$ Ocupación de aulas',
-                data: [
-            <?/*php foreach($data1 as $d):?>
-                <?php echo $d->val;?>, 
-            <?php endforeach; */?>
-                ],
-                backgroundColor: "#3898db",
-                borderColor: "#9b59b6",
-                borderWidth: 2
-            }]
-        };
-    var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        };
-    var grafico1 = new Chart(ctx1, {
-        type: 'bar',
-        data: data1,
-        options: options
-    });
-</script>
--->
         <div class="container">
             
             <br>
